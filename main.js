baf5da4e8e760436b87ea3495b5e5a41cd62a516
robbinhan <luckyhanbin@gmail.com>
2015-10-24 15:44:24 +0800

    不设置代理的版本

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
-var Agent = require('socks5-https-client/lib/Agent');
 var request = require('request');
 var http = require('http');
 var url = require('url');
-var socksHost = '127.0.0.1';
-var socksPort = 7070;
 
 http.createServer(function (req, res) {
-         var srvUrl = url.parse(req.url,true);
-         var q = srvUrl.query.url
-         var num = srvUrl.query.num || 10;
+   var srvUrl = url.parse(req.url,true);
+   var q = srvUrl.query.url
+   var num = srvUrl.query.num || 10;
 
-         /*  Use the randomNum and the time to invalidate the Google cache and 
-              fetch the latest articles.
-         */
-         var randomNum = new Date().getTime() + Math.floor((Math.random() * 10000) + 1);
-         q = encodeURIComponent(q );
+   /*  Use the randomNum and the time to invalidate the Google cache and 
+        fetch the latest articles.
+   */
+   var randomNum = new Date().getTime() + Math.floor((Math.random() * 10000) + 1);
+   q = encodeURIComponent(q);
 
+   parseFeed(q, num, function(err,feeds){
+      jsonFeeds = JSON.parse(feeds)
+      if (jsonFeeds.responseStatus != 200) {
          parseFeed(q, num, function(err,feeds){
-            jsonFeeds = JSON.parse(feeds)
-            console.log('staus',jsonFeeds.responseStatus)
-            if (jsonFeeds.responseStatus != 200) {
-                  parseFeed(q, num, function(err,feeds){
-                           res.writeHead(200, {'Content-Type': 'text/plain'});
-                           res.end(feeds);
-                  })
-            } else {
                   res.writeHead(200, {'Content-Type': 'text/plain'});
                   res.end(feeds);
-            }
          })
+      } else {
+         res.writeHead(200, {'Content-Type': 'text/plain'});
+         res.end(feeds);
+      }
+   })
 }).listen(9222);
 
 console.log('Server running on port 9222.');
@@ -36,7 +32,6 @@ console.log('Server running on port 9222.');
 
 function parseFeed (q,num,callback) {
       apiUrl = 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num='+ num+ '&q='+q;
-      console.log(apiUrl);
       request({
             headers: {
                'Cache-Control': 'max-age=0'
@@ -44,13 +39,7 @@ function parseFeed (q,num,callback) {
             timeout: 60000,
             url: apiUrl,
             strictSSL: true,
-            agentClass: Agent,
-            agentOptions: {
-                  socksHost: socksHost,
-                  socksPort: socksPort
-            }
       }, function(err, res) {
-                  // console.log(err,res.body);
-                  callback(err,res.body);
+         callback(err,res.body);
       });
 }
